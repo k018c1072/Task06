c8df98aa230b3514f92d6556108c402b90f8922f
k018c1072 <k018c1072@it-neec.jp>
2020-06-03 17:46:41 +0900

    Completed w1

---
+++ Exam-6_2.py
@@ -0,0 +1,2 @@
+min, max = (6, 18)
+print(min, '歳以上', max, '歳以下')
---
+++ Exam-6_3.py
@@ -0,0 +1,4 @@
+week = {'日': 'Sun', '月': 'Mon', '火': 'Tue',
+        '水': 'Wed', '木': 'Thu', '金': 'Fri', '土': 'Sat'}
+print('【OPEN】', week['月'], '〜', week['金'])
+print('【CLOSE】', week['土'], ',', week['日'])
